feeceed57a5c4717d1df352c906609c47ad84dac
jeddy3 <jeddy3@users.noreply.github.com>
2016-04-07 11:27:18 +0100

    Add declaration-block-no-ignored-properties

--- index.js
+++ index.js
@@ -21,6 +21,7 @@ module.exports = {
     "comment-whitespace-inside": "always",
     "declaration-bang-space-after": "never",
     "declaration-bang-space-before": "always",
+    "declaration-block-no-ignored-properties": true,
     "declaration-block-no-shorthand-property-overrides": true,
     "declaration-block-semicolon-newline-after": "always-multi-line",
     "declaration-block-semicolon-space-after": "always-single-line",
