stylelint/stylelint-config-standard

commit 1ab75f05f1e1a23081295c3ba4608632560282ea

Add ignore: "consecutive-duplicates-with-different-values" option for declaration-block-no-duplicate-properties (#43)

index.js
@@ -40,7 +40,9 @@ module.exports = {
     "declaration-bang-space-after": "never",
     "declaration-bang-space-before": "always",
     "declaration-block-no-duplicate-properties": true,
-    "declaration-block-no-ignored-properties": true,
+    "declaration-block-no-ignored-properties": [ true, {
+      ignore: ["consecutive-duplicates-with-different-values"],
+    } ],
     "declaration-block-no-redundant-longhand-properties": true,
     "declaration-block-no-shorthand-property-overrides": true,
     "declaration-block-semicolon-newline-after": "always-multi-line",

commit 1e6a659e5046087e1103d46a1a8f63275456c5cf

Fix for accidental mistake in commit 1ab75f05
Commit 1ab75f05 has accidentally changed the wrong rule.

index.js
@@ -39,10 +39,10 @@ module.exports = {
     } ],
     "declaration-bang-space-after": "never",
     "declaration-bang-space-before": "always",
-    "declaration-block-no-duplicate-properties": true,
-    "declaration-block-no-ignored-properties": [ true, {
+    "declaration-block-no-duplicate-properties": [ true, {
       ignore: ["consecutive-duplicates-with-different-values"],
     } ],
+    "declaration-block-no-ignored-properties": true,
     "declaration-block-no-redundant-longhand-properties": true,
     "declaration-block-no-shorthand-property-overrides": true,
     "declaration-block-semicolon-newline-after": "always-multi-line",