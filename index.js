ba5066c6555a5603b2a96f1d3badd6e4ddf55a8a
jeddy3 <jeddy3@users.noreply.github.com>
2020-02-08 20:58:29 -0500

    Add value-keyword-case (#136)
    * Add value-keyword-case

--- index.js
+++ index.js
@@ -101,6 +101,7 @@ module.exports = {
     "selector-pseudo-element-colon-notation": "double",
     "selector-type-case": "lower",
     "unit-case": "lower",
+    "value-keyword-case": "lower",
     "value-list-comma-newline-after": "always-multi-line",
     "value-list-comma-space-after": "always-single-line",
     "value-list-comma-space-before": "never",
