708cb8a4b53b82fc0e2e982908c988a2b539541d
Richard Hallows <jeddy3@users.noreply.github.com>
2020-04-03 17:06:48 +0100

    Use standard stylelint org tooling (#142)
    * Use standard stylelint org tooling
    
    * Fix job name

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,84 +1,82 @@
-"use strict"
+"use strict";
 
-const config = require("../")
-const fs = require("fs")
-const stylelint = require("stylelint")
+const config = require("../");
+const fs = require("fs");
+const stylelint = require("stylelint");
 
-const validCss = fs.readFileSync("./__tests__/css-valid.css", "utf-8")
-const invalidCss = fs.readFileSync("./__tests__/css-invalid.css", "utf-8")
+const validCss = fs.readFileSync("./__tests__/css-valid.css", "utf-8");
+const invalidCss = fs.readFileSync("./__tests__/css-invalid.css", "utf-8");
 
 describe("flags no warnings with valid css", () => {
-  let result
+  let result;
 
   beforeEach(() => {
     result = stylelint.lint({
       code: validCss,
       config,
-    })
-  })
+    });
+  });
 
   it("did not error", () => {
-    return result.then(data => (
-      expect(data.errored).toBeFalsy()
-    ))
-  })
+    return result.then((data) => expect(data.errored).toBeFalsy());
+  });
 
   it("flags no warnings", () => {
-    return result.then(data => (
+    return result.then((data) =>
       expect(data.results[0].warnings).toHaveLength(0)
-    ))
-  })
-})
+    );
+  });
+});
 
 describe("flags warnings with invalid css", () => {
-  let result
+  let result;
 
   beforeEach(() => {
     result = stylelint.lint({
       code: invalidCss,
       config,
-    })
-  })
+    });
+  });
 
   it("did error", () => {
-    return result.then(data => (
-      expect(data.errored).toBeTruthy()
-    ))
-  })
+    return result.then((data) => expect(data.errored).toBeTruthy());
+  });
 
   it("flags one warning", () => {
-    return result.then(data => (
+    return result.then((data) =>
       expect(data.results[0].warnings).toHaveLength(1)
-    ))
-  })
+    );
+  });
 
   it("correct warning text", () => {
-    return result.then(data => (
-      expect(data.results[0].warnings[0].text).toBe("Expected a leading zero (number-leading-zero)")
-    ))
-  })
+    return result.then((data) =>
+      expect(data.results[0].warnings[0].text).toBe(
+        "Expected a leading zero (number-leading-zero)"
+      )
+    );
+  });
 
   it("correct rule flagged", () => {
-    return result.then(data => (
+    return result.then((data) =>
       expect(data.results[0].warnings[0].rule).toBe("number-leading-zero")
-    ))
-  })
+    );
+  });
 
   it("correct severity flagged", () => {
-    return result.then(data => (
+    return result.then((data) =>
       expect(data.results[0].warnings[0].severity).toBe("error")
-    ))
-  })
+    );
+  });
 
   it("correct line number", () => {
-    return result.then(data => (
+    return result.then((data) =>
       expect(data.results[0].warnings[0].line).toBe(2)
-    ))
-  })
+    );
+  });
 
   it("correct column number", () => {
-    return result.then(data => (
+    return result.then((data) =>
       expect(data.results[0].warnings[0].column).toBe(8)
-    ))
-  })
-})
+    );
+  });
+});
