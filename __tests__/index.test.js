b6109c449cae463be2ac796664a113e7ae9f3fbb
jeddy3 <jeddy3@users.noreply.github.com>
2021-10-20 16:51:12 +0100

    Rename fixtures

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -4,8 +4,8 @@ const config = require('../');
 const fs = require('fs');
 const stylelint = require('stylelint');
 
-const validCss = fs.readFileSync('./__tests__/css-valid.css', 'utf-8');
-const invalidCss = fs.readFileSync('./__tests__/css-invalid.css', 'utf-8');
+const validCss = fs.readFileSync('./__tests__/valid.css', 'utf-8');
+const invalidCss = fs.readFileSync('./__tests__/invalid.css', 'utf-8');
 
 describe('flags no warnings with valid css', () => {
 	let result;
