dd1ce5c7335801182f51aa609d6398b82c6d57d1
Masafumi Koba <473530+ybiquitous@users.noreply.github.com>
2022-10-15 21:40:41 +0900

    Add custom parameter messages to `*-pattern` rules (#264)

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -4,69 +4,76 @@ const config = require('../');
 const fs = require('fs');
 const stylelint = require('stylelint');
 
-const validCss = fs.readFileSync('./__tests__/valid.css', 'utf-8');
-const invalidCss = fs.readFileSync('./__tests__/invalid.css', 'utf-8');
-
 describe('flags no warnings with valid css', () => {
+	const validCss = fs.readFileSync('./__tests__/valid.css', 'utf-8');
 	let result;
 
-	beforeEach(() => {
-		result = stylelint.lint({
+	beforeEach(async () => {
+		result = await stylelint.lint({
 			code: validCss,
 			config,
 		});
 	});
 
 	it('did not error', () => {
-		return result.then((data) => expect(data.errored).toBeFalsy());
+		expect(result.errored).toBe(false);
 	});
 
 	it('flags no warnings', () => {
-		return result.then((data) => expect(data.results[0].warnings).toHaveLength(0));
+		expect(result.results[0].warnings).toHaveLength(0);
 	});
 });
 
 describe('flags warnings with invalid css', () => {
+	const invalidCss = fs.readFileSync('./__tests__/invalid.css', 'utf-8');
 	let result;
 
-	beforeEach(() => {
-		result = stylelint.lint({
+	beforeEach(async () => {
+		result = await stylelint.lint({
 			code: invalidCss,
 			config,
 		});
 	});
 
 	it('did error', () => {
-		return result.then((data) => expect(data.errored).toBeTruthy());
+		expect(result.errored).toBe(true);
 	});
 
-	it('flags one warning', () => {
-		return result.then((data) => expect(data.results[0].warnings).toHaveLength(1));
+	it('flags warnings', () => {
+		expect(result.results[0].warnings).toHaveLength(6);
 	});
 
 	it('correct warning text', () => {
-		return result.then((data) =>
-			expect(data.results[0].warnings[0].text).toBe(
-				'Expected a leading zero (number-leading-zero)',
-			),
-		);
+		expect(result.results[0].warnings.map((w) => w.text)).toEqual([
+			'Expected custom media query name "--FOO" to be kebab-case',
+			'Expected custom property name "--FOO" to be kebab-case',
+			'Expected keyframe name "FOO" to be kebab-case',
+			'Expected a leading zero (number-leading-zero)',
+			'Expected class selector ".FOO" to be kebab-case',
+			'Expected id selector "#FOO" to be kebab-case',
+		]);
 	});
 
 	it('correct rule flagged', () => {
-		return result.then((data) =>
-			expect(data.results[0].warnings[0].rule).toBe('number-leading-zero'),
-		);
+		expect(result.results[0].warnings.map((w) => w.rule)).toEqual([
+			'custom-media-pattern',
+			'custom-property-pattern',
+			'keyframes-name-pattern',
+			'number-leading-zero',
+			'selector-class-pattern',
+			'selector-id-pattern',
+		]);
 	});
 
 	it('correct severity flagged', () => {
-		return result.then((data) => expect(data.results[0].warnings[0].severity).toBe('error'));
+		expect(result.results[0].warnings[0].severity).toBe('error');
 	});
 
 	it('correct line number', () => {
-		return result.then((data) => expect(data.results[0].warnings[0].line).toBe(2));
+		expect(result.results[0].warnings[0].line).toBe(5);
 	});
 
 	it('correct column number', () => {
-		return result.then((data) => expect(data.results[0].warnings[0].column).toBe(8));
+		expect(result.results[0].warnings[0].column).toBe(15);
 	});
 });
