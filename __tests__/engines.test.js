1839564595c6b349c74e523ec94d49d7e8b3809d
Masafumi Koba <473530+ybiquitous@users.noreply.github.com>
2023-06-19 22:08:35 +0900

    Add `engines.node` field to `package.json`
    The `engines.node` field should be the same as `stylelint`'s `engines.node` (peer dependency).

---
+++ __tests__/engines.test.js
@@ -0,0 +1,20 @@
+'use strict';
+
+const { execFileSync } = require('child_process');
+const pkg = require('../package.json');
+
+describe('engines.node', () => {
+	it("is the same as stylelint's one", () => {
+		const stylelintVersion = pkg.peerDependencies.stylelint;
+		const [nodeVersion] = JSON.parse(
+			execFileSync('npm', [
+				'view',
+				'--json',
+				`stylelint@${stylelintVersion}`,
+				'engines.node',
+			]).toString(),
+		);
+
+		expect(nodeVersion).toEqual(pkg.engines.node);
+	});
+});
