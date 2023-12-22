7e45b0bd12913d7c8c889e0273d0bdab273b4a2f
Masafumi Koba <473530+ybiquitous@users.noreply.github.com>
2023-12-22 10:07:29 +0900

    Fix ESLint issues

--- __tests__/engines.test.mjs
+++ __tests__/engines.test.mjs
@@ -1,4 +1,4 @@
-import { describe, it, beforeEach } from 'node:test';
+import { describe, it } from 'node:test';
 import assert from 'node:assert/strict';
 import { execFileSync } from 'node:child_process';
 import { readFileSync } from 'node:fs';
